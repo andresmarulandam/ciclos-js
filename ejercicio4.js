c41e82ae7dcdbf0cd6e2aff1bfa80ae8e1a19855
Andres <marulandaandres19@gmail.com>
2023-03-13 18:00:33 -0500

    otros ejercicios

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,1 +1,4 @@
-// Imprimir los números del 100 al 0 en orden descendente en la consola.
+// Imprimir los números del 100 al 0 en orden descendente en la consola
+for ( let i = 100; i >=0; i-- ){
+    console.log(i)
+}
---
+++ ejercicio5.js
@@ -0,0 +1,14 @@
+// Crear una función que acepte un número como parámetro y devuelva la suma de todos los números enteros desde 1 hasta ese número.
+
+function sum (numberUser){
+    let numero1=0
+    let numero2=1
+    let numero3= numero1 +numero2
+
+    for ( let 1 = numero3; i<=numberUser; i++ ){
+        
+    }
+    
+    
+}
+console.log(sum(15))
