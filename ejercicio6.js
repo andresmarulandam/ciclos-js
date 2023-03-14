65370f24b5fcf7f159a02c0c131c6765745b8519
Andres <marulandaandres19@gmail.com>
2023-03-14 18:50:49 -0500

    nuevos ejercicios

---
+++ ejercicio6.js
@@ -0,0 +1,9 @@
+// Crear un programa que imprima en la consola 50 veces el texto Estoy aprendiendo. Cada vez que se imprime el texto, debe aparecer al inicio el número de veces que se ha impreso:
+
+for (let i= 1; i<=50; i++) {
+    console.log( i + ' Estoy aprendiendo')
+
+}
+
+
+
---
+++ ejercicio7.js
@@ -0,0 +1,7 @@
+// Corregir el siguiente código para que se imprima 5 veces el texto que se encuentra dentro del cuerpo del ciclo.
+// while (false) { console.log("Hola desde el cuerpo del ciclo");}
+
+for ( let i=0; i<=5; i++) {
+
+console.log("Hola desde el cuerpo del ciclo")
+}
